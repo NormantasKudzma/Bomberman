82558548e98aa710a473df6f906bdc3e2ff830c1
Hajama <ASASIS1@gmail.com>
2015-10-11 11:46:18 +0300

    pim

--- src/game/PlayerEntity.java
+++ src/game/PlayerEntity.java
@@ -20,80 +20,88 @@ public class PlayerEntity extends Entity {
 	private long s;
 
 	@Override
-	public void update(float deltaTime){
-		if(moveDirection != null){
-		   getPosition().add(moveDirection);
-		   moveDirection.set(0, 0);
+	public void update(float deltaTime) {
+		if (moveDirection != null) {
+			getPosition().add(moveDirection);
+			moveDirection.set(0, 0);
 		}
 	}
-	public void readKeybindings(){
-		keyboard = ControllerManager.getInstance()
-			    .getController(EController.LWJGLKEYBOARDCONTROLLER);
+
+	public void readKeybindings() {
+		keyboard = ControllerManager.getInstance().getController(
+				EController.LWJGLKEYBOARDCONTROLLER);
 		Scanner sc2 = null;
-	    try {
-	        sc2 = new Scanner(new File(Paths.CONFIGS + "DefaultKeybinds"));
-	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();  
-	    }
-	    while (sc2.hasNextLine()) {
-	            Scanner s2 = new Scanner(sc2.nextLine());
-	            int counter = 1;
-	        while (s2.hasNext()) {
-	        	if(counter % 2 != 0 && counter == 1) {
-	        	   s = s2.nextLong();
-	        	   String m = s2.next();
-	        	}   
-	        	if(counter % 2 != 0 && counter == 3) {
-	        		String s1 = s2.next();
-	        		try {
-	        		    method = this.getClass().getMethod(s1);
-	        		} catch (SecurityException e) {
-	        		  // ...
-	        		} catch (NoSuchMethodException e) {
-	        		  // ...
-	        		}
-	        	}	
-	        	counter += 1;
-	        }
-	        keyboard.addKeybind(new ControllerKeybind(s, new K1(method, this)));
-	    }
-	    keyboard.startController();
+		try {
+			sc2 = new Scanner(new File(Paths.CONFIGS + "DefaultKeybinds"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (sc2.hasNextLine()) {
+			Scanner s2 = new Scanner(sc2.nextLine());
+			int counter = 1;
+			while (s2.hasNext()) {
+				if (counter % 2 != 0 && counter == 1) {
+					s = s2.nextLong();
+					String m = s2.next();
+				}
+				if (counter % 2 != 0 && counter == 3) {
+					String s1 = s2.next();
+					try {
+						method = this.getClass().getMethod(s1);
+					} catch (SecurityException e) {
+						// ...
+					} catch (NoSuchMethodException e) {
+						// ...
+					}
+				}
+				counter += 1;
+			}
+			keyboard.addKeybind(new ControllerKeybind(s, new K1(method, this)));
+		}
+		keyboard.startController();
 	}
-	public void moveUp(){
+
+	public void moveUp() {
 		moveDirection.setY(0.0002f);
 	}
-	public void moveDown(){
+
+	public void moveDown() {
 		moveDirection.setY(-0.0002f);
 	}
-	public void moveLeft(){
-		moveDirection.setX(-0.0002f);	
+
+	public void moveLeft() {
+		moveDirection.setX(-0.0002f);
 	}
-	public void moveRight(){
+
+	public void moveRight() {
 		moveDirection.setX(0.0002f);
 	}
+
 	class K1 implements ControllerEventListener {
 		java.lang.reflect.Method metodas;
 		PlayerEntity player;
-		public K1(java.lang.reflect.Method m, PlayerEntity p){
+
+		public K1(java.lang.reflect.Method m, PlayerEntity p) {
 			player = p;
 			metodas = m;
 		}
+
 		@Override
 		public void handleEvent(long mask) {
-				try {
-					metodas.invoke(player);
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			try {
+				metodas.invoke(player);
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-              
+
 	}
 
 }
